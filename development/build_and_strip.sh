0143b51f5573009a0b48563dba70edf93ca3d1ae
Dave Allie <dave@daveallie.com>
2018-09-08 18:11:04 +1000

    Update development scripts

--- development/build_and_strip.sh
+++ development/build_and_strip.sh
@@ -5,23 +5,37 @@ set -e
 cd "$(dirname $0)"
 cd ..
 
-rm ./target/release/grapple &>/dev/null || true
-FULL_TOOLCHAIN="$(rustup toolchain list | grep default | awk '{print $1}' | cut -d '-' -f2-)"
-cargo build --release
+case `uname -s` in
+    Linux)
+        FULL_TOOLCHAIN="x86_64-unknown-linux-gnu"
+        ;;
+    Darwin)
+        FULL_TOOLCHAIN="x86_64-apple-darwin"
+        ;;
+    *)
+        FULL_TOOLCHAIN="$(rustup toolchain list | grep default | awk '{print $1}' | cut -d '-' -f2-)"
+        ;;
+esac
 
-echo "Before Strip: $(ls -lh ./target/release/grapple | awk '{print $5}')"
-strip ./target/release/grapple
-echo " After Strip: $(ls -lh ./target/release/grapple | awk '{print $5}')"
+RELEASE_FOLDER="./target/${FULL_TOOLCHAIN}/release"
+BINARY_PATH="${RELEASE_FOLDER}/grapple"
+ZIP_FILE="grapple-$(git rev-parse --short HEAD)-${FULL_TOOLCHAIN}.tar.gz"
+ZIP_PATH="${RELEASE_FOLDER}/${ZIP_FILE}"
+PACKAGE_FOLDER="./pkg"
+FINAL_ZIP_PATH="${PACKAGE_FOLDER}/${ZIP_FILE}"
 
-VERSION="$(./target/release/grapple -V | awk '{print $2}')"
-FILENAME="grapple-$VERSION-$FULL_TOOLCHAIN.tar.gz"
+rm ${BINARY_PATH} &>/dev/null || true
+rm ${ZIP_PATH} &>/dev/null || true
+cargo build --release --target ${FULL_TOOLCHAIN}
 
-cd ./target/release
-tar -zcf $FILENAME ./grapple
-echo "GZipped File: $(ls -lh $FILENAME | awk '{print $5}')"
+echo "Before Strip: $(ls -lh ${BINARY_PATH} | awk '{print $5}')"
+strip ${BINARY_PATH}
+echo " After Strip: $(ls -lh ${BINARY_PATH} | awk '{print $5}')"
 
-cd ../..
-mkdir -p ./pkg
-mv ./target/release/$FILENAME ./pkg/
+tar -zcf ${ZIP_PATH} ${BINARY_PATH}
+echo "GZipped File: $(ls -lh ${ZIP_PATH} | awk '{print $5}')"
 
-echo "Built and zipped to ./pkg/$FILENAME"
+mkdir -p ${PACKAGE_FOLDER}
+mv ${ZIP_PATH} ${FINAL_ZIP_PATH}
+
+echo "Built and zipped to ${FINAL_ZIP_PATH}"
